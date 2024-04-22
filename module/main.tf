31d256be1bc9c41f39b2191b60aed4fa13f5e344
VikasVashisth <vikas.vashisth@nashtechglobal.com>
2024-04-22 17:43:43 +0530

    updated variables

--- module/main.tf
+++ module/main.tf
@@ -1,21 +1,21 @@
 data "azurerm_network_security_group" "nsg" {
-  name                = var.name
-  resource_group_name = "${var.name}-rg"
+  name                = var.name_VV
+  resource_group_name = "${var.name_VV}-rg"
 }
 
 
 resource "azurerm_network_security_rule" "network_security_rule" {
 
-  name                         = "${var.network_security_rule_access}-${var.network_security_rule_direction}-${var.network_security_rule_protocol}-${var.network_security_rule_priority}"
-  priority                     = var.network_security_rule_priority
-  direction                    = var.network_security_rule_direction
-  access                       = var.network_security_rule_access
-  protocol                     = var.network_security_rule_protocol
-  source_port_ranges           = var.network_security_rule_source_port_ranges
-  destination_port_ranges      = var.network_security_rule_destination_port_ranges
-  source_address_prefixes      = var.network_security_rule_source_address_prefixes
-  destination_address_prefixes = var.network_security_rule_destination_address_prefixes
-  resource_group_name          = data.azurerm_network_security_group.nsg.resource_group_name
-  network_security_group_name  = data.azurerm_network_security_group.nsg.name
+  name                         = "${var.network_security_rule_access_VV}-${var.network_security_rule_direction_VV}-${var.network_security_rule_protocol_VV}-${var.network_security_rule_priority_VV}"
+  priority                     = var.network_security_rule_priority_VV
+  direction                    = var.network_security_rule_direction_VV
+  access                       = var.network_security_rule_access_VV
+  protocol                     = var.network_security_rule_protocol_VV
+  source_port_ranges           = var.network_security_rule_source_port_ranges_VV
+  destination_port_ranges      = var.network_security_rule_destination_port_ranges_VV
+  source_address_prefixes      = var.network_security_rule_source_address_prefixes_VV
+  destination_address_prefixes = var.network_security_rule_destination_address_prefixes_VV
+  resource_group_name          = data.azurerm_network_security_group.nsg.resource_group_name_VV
+  network_security_group_name  = data.azurerm_network_security_group.nsg.name_VV
 
 }
--- module/variables.tf
+++ module/variables.tf
@@ -1,46 +1,46 @@
-variable "name" {
+variable "name_VV" {
   description = "Name of Network Security Group"
   type        = string
 }
 
-variable "network_security_rule_name" {
+variable "network_security_rule_name_VV" {
   description = "Name of Network Security Rule, it is consists of the combination of access-direction-protocol-priority."
   type        = string
 }
-variable "network_security_rule_priority" {
+variable "network_security_rule_priority_VV" {
   description = "Priority of Network Security Rule"
   type        = number
 }
-variable "network_security_rule_direction" {
+variable "network_security_rule_direction_VV" {
   description = "Direction of Network Security Rule. Possible values are Inbound and Outbound."
   type        = string
   default     = "Inbound"
 }
-variable "network_security_rule_access" {
+variable "network_security_rule_access_VV" {
   description = "Access of Network Security Rule"
   type        = string
   default     = "Allow"
 }
-variable "network_security_rule_protocol" {
+variable "network_security_rule_protocol_VV" {
   description = "Protocol of Network Security Rule"
   type        = string
   default     = "Tcp"
 }
-variable "network_security_rule_source_port_ranges" {
+variable "network_security_rule_source_port_ranges_VV" {
   description = "Source Port Ranges of Network Security Rule"
   type        = list(string)
 }
 
-variable "network_security_rule_destination_port_ranges" {
+variable "network_security_rule_destination_port_ranges_VV" {
   description = "Destination Port Ranges of Network Security Rule"
   type        = list(string)
 }
-variable "network_security_rule_source_address_prefixes" {
+variable "network_security_rule_source_address_prefixes_VV" {
   description = "Source Address Prefixes of Network Security Rule"
   type        = list(string)
 }
 
-variable "network_security_rule_destination_address_prefixes" {
+variable "network_security_rule_destination_address_prefixes_VV" {
   description = "Destination Address Prefixes of Network Security Rule"
   type        = list(string)
 }
